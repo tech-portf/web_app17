0ace2c6269141b9a76f92c2c7cfa20e29b61a36b
kitajima <kitajima@example.com>
2025-03-06 23:49:18 +0900

    all code commit

---
+++ src/main/java/com/sutaru/TutorialDevApplication.java
@@ -0,0 +1,13 @@
+package com.sutaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sutaru/TutorialDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutaru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
